4f47b0f7a1580702f8069f2a75d2afc4debc682f
Olajide Salami <olajide.salami@ibedc.com>
2026-03-26 16:00:15 +0100

    test build pipeline

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -3,7 +3,7 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME_,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
